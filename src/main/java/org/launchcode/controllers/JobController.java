6231a575818dc043b57c37a9e7ffa29e96594e31
Billy Kirchner <bkirchner27@gmail.com>
2018-12-07 12:37:51 -0600

    Final commit - all tasks complete

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -7,10 +7,7 @@ import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,8 +21,8 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String index(Model model, @PathVariable(value="id") int id) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, @RequestParam("id") int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
         model.addAttribute("job", jobData.findById(id));
@@ -46,14 +43,13 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        newJob.setName(jobForm.getName());
+
         newJob.setEmployer(jobData.findById(jobForm.getEmployerId()).getEmployer());
         newJob.setLocation(jobData.findById(jobForm.getLocationId()).getLocation());
         newJob.setPositionType(jobData.findById(jobForm.getPositionId()).getPositionType());
         newJob.setCoreCompetency(jobData.findById(jobForm.getCoreCompetencyId()).getCoreCompetency());
 
         if (errors.hasErrors()) {
-            model.addAttribute(new JobForm());
             return "new-job";
         }
 
@@ -62,7 +58,7 @@ public class JobController {
         int id = newJob.getId();
 
 
-        return "redirect:/job/" + id;
+        return "redirect:/job/?id=" + id;
 
     }
 }
